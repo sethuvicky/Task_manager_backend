d1d781bc086e463afd677c74e9b18bf52298ec6f
sethuvicky <75525385+sethuvicky@users.noreply.github.com>
2024-11-24 10:42:38 +0530

    Add files via upload

---
+++ src/controllers/auth/userController.js
@@ -0,0 +1,241 @@
+import asyncHandler from "express-async-handler";
+import User from "../../models/auth/UserModel.js";
+import generateToken from "../../helpers/generateToken.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Token from "../../models/auth/Token.js";
+import crypto from "node:crypto";
+import hashToken from "../../helpers/hashToken.js";
+ 
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //validation
+  if (!name || !email || !password) {
+    // 400 Bad Request
+    res.status(400).json({ message: "All fields are required" });
+  }
+
+  // check password length
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: "Password must be at least 6 characters" });
+  }
+
+  // check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    // bad request
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  // create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  // generate token with user id
+  const token = generateToken(user._id);
+
+  // send back the user and token in the response to the client
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    sameSite: "none", // cross-site access --> allow all third-party cookies
+    secure: false,
+  });
+
+  if (user) {
+    const { _id, name, email, role, photo, bio, isVerified } = user;
+
+    // 201 Created
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      role,
+      photo,
+      bio,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data" });
+  }
+});
+
+// user login
+export const loginUser = asyncHandler(async (req, res) => {
+  // get email and password from req.body
+  const { email, password } = req.body;
+
+  // validation
+  if (!email || !password) {
+    // 400 Bad Request
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (!userExists) {
+    return res.status(404).json({ message: "User not found, sign up!" });
+  }
+
+  // check id the password match the hashed password in the database
+  const isMatch = await bcrypt.compare(password, userExists.password);
+
+  if (!isMatch) {
+    // 400 Bad Request
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  // generate token with user id
+  const token = generateToken(userExists._id);
+
+  if (userExists && isMatch) {
+    const { _id, name, email, role, photo, bio, isVerified } = userExists;
+
+    // set the token in the cookie
+    res.cookie("token", token, {
+      path: "/",
+      httpOnly: true,
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+      sameSite: "none", // cross-site access --> allow all third-party cookies
+      secure: true,
+    });
+
+    // send back the user and token in the response to the client
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      role,
+      photo,
+      bio,
+      isVerified,
+      token,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid email or password" });
+  }
+});
+
+// logout user
+export const logoutUser = asyncHandler(async (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    path: "/",
+  });
+
+  res.status(200).json({ message: "User logged out" });
+});
+
+// get user
+export const getUser = asyncHandler(async (req, res) => {
+  // get user details from the token ----> exclude password
+  const user = await User.findById(req.user._id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    // 404 Not Found
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+// update user
+export const updateUser = asyncHandler(async (req, res) => {
+  // get user details from the token ----> protect middleware
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    // user properties to update
+    const { name, bio, photo } = req.body;
+    // update user properties
+    user.name = req.body.name || user.name;
+    user.bio = req.body.bio || user.bio;
+    user.photo = req.body.photo || user.photo;
+
+    const updated = await user.save();
+
+    res.status(200).json({
+      _id: updated._id,
+      name: updated.name,
+      email: updated.email,
+      role: updated.role,
+      photo: updated.photo,
+      bio: updated.bio,
+      isVerified: updated.isVerified,
+    });
+  } else {
+    // 404 Not Found
+    res.status(404).json({ message: "User not found" });
+  }
+});
+
+// login status
+export const userLoginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    // 401 Unauthorized
+    res.status(401).json({ message: "Not authorized, please login!" });
+  }
+  // verify the token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (decoded) {
+    res.status(200).json(true);
+  } else {
+    res.status(401).json(false);
+  }
+});
+
+ 
+
+// verify user
+export const verifyUser = asyncHandler(async (req, res) => {
+  const { verificationToken } = req.params;
+
+  if (!verificationToken) {
+    return res.status(400).json({ message: "Invalid verification token" });
+  }
+  // hash the verification token --> because it was hashed before saving
+  const hashedToken = hashToken(verificationToken);
+
+  // find user with the verification token
+  const userToken = await Token.findOne({
+    verificationToken: hashedToken,
+    // check if the token has not expired
+    expiresAt: { $gt: Date.now() },
+  });
+
+  if (!userToken) {
+    return res
+      .status(400)
+      .json({ message: "Invalid or expired verification token" });
+  }
+
+  //find user with the user id in the token
+  const user = await User.findById(userToken.userId);
+
+  if (user.isVerified) {
+    // 400 Bad Request
+    return res.status(400).json({ message: "User is already verified" });
+  }
+
+  // update user to verified
+  user.isVerified = true;
+  await user.save();
+  res.status(200).json({ message: "User verified" });
+});
+
+ 
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { getUser,
+  loginUser,
+  logoutUser,
+  registerUser,
+   updateUser,
+  userLoginStatus,
+   verifyUser,
+} from "../controllers/auth/userController.js";
+import {
+  adminMiddleware,
+  creatorMiddleware,
+  protect,
+} from "../middleware/authMiddleware.js";
+import {
+  deleteUser,
+  getAllUsers,
+} from "../controllers/auth/adminController.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/user", protect, getUser);
+router.patch("/user", protect, updateUser);
+
+// admin route
+router.delete("/admin/users/:id", protect, adminMiddleware, deleteUser);
+
+// get all users
+router.get("/admin/users", protect, creatorMiddleware, getAllUsers);
+
+// login status
+router.get("/login-status", userLoginStatus);
+
+// veriify user --> email verification
+router.post("/verify-user/:verificationToken", verifyUser);
+
+ 
+ 
+
+export default router;
